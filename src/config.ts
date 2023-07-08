bebf3e3929bfa7a9c836c333c1003a9f2795b1bb
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-07-08 21:29:32 +0930

    Inception

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Greetings, Digital Explorers!';
+export const SITE_DESCRIPTION = `Hey there! I'm Krishan, your friendly DevOps engineer, dedicated to crafting a seamless online experience for you. Join me as I explore the art of building robust infrastructures, automating deployments, and optimizing workflows.`;
+
+export const PROF_LINKEDIN = 'https://www.linkedin.com/in/krishan-thisera/'
+export const PROF_GITHUB = 'https://www.github.com/krishanthisera'
+export const PROF_TWITTER = 'https://www.twitter.com/krishanthisera'
+
+export const IMAGE_CDN_PREFIX = 'https://bizkt.imgix.net'
+
