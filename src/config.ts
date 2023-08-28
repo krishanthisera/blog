d1af76724aef39a998f39e49bf3b819264dcde4a
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-08-28 18:41:15 +0930

    run prettier globally

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Greetings, Digital Explorers!';
+export const SITE_TITLE = "Greetings, Digital Explorers!";
 export const SITE_DESCRIPTION = `Hey there! I'm Krishan, your friendly DevOps engineer, dedicated to crafting a seamless online experience for you. Join me as I explore the art of building robust infrastructures, automating deployments, and optimizing workflows.`;
 
-export const PROF_LINKEDIN = 'https://www.linkedin.com/in/krishan-thisera/'
-export const PROF_GITHUB = 'https://www.github.com/krishanthisera'
-export const PROF_TWITTER = 'https://twitter.com/Krisheeya'
+export const PROF_LINKEDIN = "https://www.linkedin.com/in/krishan-thisera/";
+export const PROF_GITHUB = "https://www.github.com/krishanthisera";
+export const PROF_TWITTER = "https://twitter.com/Krisheeya";
 
-export const IMAGE_CDN_PREFIX = 'https://bizkt.imgix.net'
-
+export const IMAGE_CDN_PREFIX = "https://bizkt.imgix.net";
